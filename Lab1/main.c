e7e35ccc76e6c58bab41c1acf4167d28753fb17c
ViktorSalakin <47810962+ViktorSalakin@users.noreply.github.com>
2019-02-20 11:24:31 +0300

    добавление первой лаборатрной

---
+++ Lab1/main.c
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include <stdio.h>
+
+int main(void)
+{
+	float r;
+	float a;
+	float S;
+	const int P = 3.14;
+
+	printf("r=");
+	scanf_s("%f=", &r);
+	printf("a=");
+	scanf_s("%f=", &a);
+	S = P * r*r - a * a;
+	printf("plosad` =%f\n", S);
+
+	return 0;
+}
